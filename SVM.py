8490c7dadcdf0bf01b0b1e7f55089be9981c6ed5
srushd <34884032+srushd@users.noreply.github.com>
2019-03-27 14:48:29 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -22,9 +22,14 @@ from sklearn.svm import SVC
 
 
 clf = SVC(kernel='linear',gamma='auto')
+t0 = time()
 clf.fit(features_train, labels_train)
+print ("\ntraining time:", round(time()-t0,3), "s")
 
-pred=clf.predict(features_test)
+# predict
+t0 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time()-t0, 3), "s")
 
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(pred, labels_test)
