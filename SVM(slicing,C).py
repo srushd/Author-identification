32956e7dc94a0bf94c96b453d70dc18fdb140c42
srushd <34884032+srushd@users.noreply.github.com>
2019-03-27 21:32:51 +0530

    Update and rename SVM.py to SVM(slicing,C).py

--- SVM(slicing,C).py
+++ SVM(slicing,C).py
@@ -21,7 +21,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 features_train = features_train[:len(features_train)//100] 
 labels_train = labels_train[:len(labels_train)//100] 
-clf = SVC(kernel='linear',gamma='auto')
+clf = SVC(C=10000,kernel='rbf',gamma='auto')
 
 t0 = time()
 
