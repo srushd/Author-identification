bb09e9aa0a3aae4a98995c0e5ef210872d656525
srushd <34884032+srushd@users.noreply.github.com>
2019-05-16 02:17:52 +0530

    Update decision tree classifier.py

--- decision tree classifier.py
+++ decision tree classifier.py
@@ -1,41 +1,17 @@
-#!/usr/bin/python
-from sklearn.model_selection import train_test_split
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
-import sys
 from time import time
-sys.path.append("C:/Users/Srushti/Desktop/New folder (3)/ML udacity/ud120-projects-master/tools")
-#from email_preprocess import preprocess
-
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-clf = DecisionTreeClassifier(min_samples_split=40)
+clf= DecisionTreeClassifier(min_samples_split=40)
 clf.fit(features_train,labels_train)
+t0=time()
 pred=clf.predict(features_test)
+
+print('predicting time: ', round(time()-t0,3),'s')
 
 
 
 accuracy = accuracy_score(pred, labels_test)
 
 print ('\naccuracy = {0}'.format(accuracy))
-    
+len(features_train[0])    
